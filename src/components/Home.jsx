2813853033888199374487be16b6736c67b6dcbb
alessiobastiani <ale-ssio@hotmail.es>
2024-01-11 19:32:00 -0300

    bonita

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <img className='desing' src={desing} alt="" />
             <div className='texto-intro'>
                 <h1>Manicuria profesional</h1>
-                <p>En margarita nos apasiona realzar tu belleza a través de nuestras habilidades en manicura. En cada detalle, desde la elección de colores hasta la precisión en la forma, trabajamos con amor y dedicación. Tu felicidad es la nuestra, y queremos que cada visita a nuestro salón de uñas sea una experiencia que nunca olvidarás</p>
+                <p>En Bonita nos apasiona realzar tu belleza a través de nuestras habilidades en manicura. En cada detalle, desde la elección de colores hasta la precisión en la forma, trabajamos con amor y dedicación. Tu felicidad es la nuestra, y queremos que cada visita a nuestro salón de uñas sea una experiencia que nunca olvidarás</p>
                 <Link to= "/conocenos">
                 <button className='btn-conocenos btn'>Conocenos</button>
                 </Link>
